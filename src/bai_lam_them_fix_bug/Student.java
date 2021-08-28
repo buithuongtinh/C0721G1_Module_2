d38f08e0a67b730e6541de46f361e187eb9af3b1
tinh <thuongtinh2000@gmail.com>
2021-08-28 09:44:13 +0700

    fix bug 2

---
+++ src/bai_lam_them_fix_bug/Student.java
@@ -0,0 +1,33 @@
+package bai_lam_them_fix_bug;
+
+public  class Student extends Person {
+    private static String school = "Codegym";
+
+    public Student(String name, int AGE, Address address) {
+        super(name, AGE, address);
+    }
+
+    public static String getSchool() {
+        return school;
+    }
+
+    public static void setSchool(String school) {
+        Student.school = school;
+    }
+
+    @Override
+    public void howToMove() {
+        System.out.println("Student goes by any vehicle");
+    }
+
+//    @Override
+//    public String toString() {
+//        return "Student" +super.toString() +"\nschool"+school;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "Student : " + super.toString() + ",school: "+ school;
+    }
+}
---
+++ src/bai_lam_them_fix_bug/Teacher.java
@@ -0,0 +1,30 @@
+package bai_lam_them_fix_bug;
+
+public class Teacher extends Person {
+    private int salary;
+
+    public Teacher(int salary,String name, int AGE, Address address) {
+        super(name, AGE, address);
+        this.salary=salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public void howToMove() {
+        System.out.println("Teacher goes by motorbike");
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +super.toString()+
+                "salary=" + salary +
+                '}';
+    }
+}
